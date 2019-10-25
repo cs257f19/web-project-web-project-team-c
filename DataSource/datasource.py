5b54e89de5bfafb14370eb4c38fa8d3c79ab7ccf
Louis Ye <yel@carleton.edu>
2019-10-24 21:32:48 -0500

    getDataInRange writing

--- DataSource/datasource.py
+++ DataSource/datasource.py
@@ -28,18 +28,24 @@ class DataSource:
             exit()
         return connection
 
-    def getDataInRange(self, start, end=10.0):
+    def getDataInRange(self, dataset, fromDate, toDate= "2019-10-09"):
         '''
-        Returns a list of all of the magnitudes from the specified starting magnitude until the specified ending magnitude.
+        Returns a list containing data within given range for specified dataset
 
-        PARAMETERS:
-            start - the low end of the magnitude range
-            end - the high end of the magnitude range (default: 10.0)
-
-        RETURN:
-            a list of all of the earthquake events with magnitudes in the specified range
+        Returns: a list of all data within range.
         '''
-        return []
+
+
+
+        try:
+            cursor = connection.cursor()
+            query = "SELECT	* FROM " + dataset + " WHERE (pricedate BETWEEN '" + fromDate + "' AND '" + toDate + "')"
+            cursor.execute(query)
+            return cursor.fetchall()
+
+        except Exception as e:
+            print("Something went wrong when executing the query: ", e)
+            return None
 
     def getQuakesOnContinent(self, continent):
         '''

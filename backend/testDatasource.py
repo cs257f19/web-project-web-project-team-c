da632a8df28ddda6f25e765d9bae50b6e6c01a4f
Louis Ye <yel@carleton.edu>
2019-10-29 14:23:23 -0500

    Got one unitTest to work. Need to comment out the connect function though.

--- backend/testDatasource.py
+++ backend/testDatasource.py
@@ -7,8 +7,7 @@ class DataSourceTester(unittest.TestCase):
     def test_dateToTimeCoversion(self):
         ds = DataSource("User", "Password")
         x = datetime.datetime(2020, 5, 17)
-        self.assertEqual(20200517, datasource.)
-
+        self.assertEqual(20200517, ds.dateTimeToInt(x))
 
 if __name__ == '__main__':
     unittest.main()

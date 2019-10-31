baa068f61f3c7a5d5362886bd3def87288390223
nhayes-rich <nhayesrich@gmail.com>
2019-10-31 15:13:55 -0500

    unit test for getDataInRange

--- backend/testDatasource.py
+++ backend/testDatasource.py
@@ -7,14 +7,19 @@ class DataSourceTester(unittest.TestCase):
     def setUp(self):
         self.ds = DataSource("hayesrichn","orange227blue")
     def test_dateToIntCoversion(self):
-        x = datetime.datetime(2020, 5, 17)
-        self.assertEqual(20200517, self.ds.dateTimeToInt(x))
+        result = datetime.datetime(2020, 5, 17)
+        self.assertEqual(20200517, self.ds.dateTimeToInt(result))
     def test_not_datetime_dateToIntConversion(self):
-        x = "hello"
-        self.assertEqual(0, self.ds.dateTimeToInt(x))
+        result = "hello"
+        self.assertEqual(0, self.ds.dateTimeToInt(result))
     def test_get_data(self):
-        x = (datetime.date(2010, 7, 17), 0.04951, 0.04951, 0.04951, 0.04951, 0.04951, 0)
-        self.assertEqual(x, self.ds.getData("btc")[0])
+        result = (datetime.date(2010, 7, 17), 0.04951, 0.04951, 0.04951, 0.04951, 0.04951, 0)
+        self.assertEqual(result, self.ds.getData("btc")[0])
+    def test_get_data_in_range(self):
+        result = [(datetime.date(2010, 7, 17), 0.04951, 0.04951, 0.04951, 0.04951, 0.04951, 0), (datetime.date(2010, 7, 18), 0.04951, 0.08585, 0.05941, 0.08584, 0.08584, 5)]
+        dataset = self.ds.getData("btc")
+        self.assertEqual(result, self.ds.getDataInRange(dataset, 20100717, 20100718))
+
 
 if __name__ == '__main__':
     unittest.main()

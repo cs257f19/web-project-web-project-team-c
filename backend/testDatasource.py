c689d320da055302e1f3b0db06906f103f431fd6
nhayes-rich <nhayesrich@gmail.com>
2019-10-31 15:02:30 -0500

    unit test setup and proper database connection

--- backend/testDatasource.py
+++ backend/testDatasource.py
@@ -4,14 +4,14 @@ import datetime
 
 
 class DataSourceTester(unittest.TestCase):
+    def setUp(self):
+        self.ds = DataSource("hayesrichn","orange227blue")
     def test_dateToIntCoversion(self):
-        ds = DataSource("User", "Password")
         x = datetime.datetime(2020, 5, 17)
-        self.assertEqual(20200517, ds.dateTimeToInt(x))
+        self.assertEqual(20200517, self.ds.dateTimeToInt(x))
     def test_not_datetime_dateToIntConversion(self):
-        ds = DataSource("User", "Password")
         x = "hello"
-        self.assertEqual(0, ds.dateTimeToInt(x))
+        self.assertEqual(0, self.ds.dateTimeToInt(x))
 
 if __name__ == '__main__':
     unittest.main()

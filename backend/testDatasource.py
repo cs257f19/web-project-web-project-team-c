fd87782d8692b8d5edd58e9b586b2b675b1b0798
nhayes-rich <nhayesrich@gmail.com>
2019-11-06 10:30:31 -0600

    rewriting unit tests for new methods

--- backend/testDatasource.py
+++ backend/testDatasource.py
@@ -6,8 +6,7 @@ import datetime
 class DataSourceTester(unittest.TestCase):
     def setUp(self):
         self.ds = DataSource("hayesrichn","orange227blue")
-        self.datasetRange = self.ds.getData("btc")
-        self.datasetType = self.ds.getDataInRange(self.datasetRange, 20100717, 20100718)
+        self.dataset = self.ds.getData("btc", "openprice", 20100717, 20100718)
 
     def test_date_to_int(self):
         result = datetime.date(2020, 5, 17)
@@ -20,14 +19,6 @@ class DataSourceTester(unittest.TestCase):
     def test_get_data(self):
         result = (datetime.date(2010, 7, 17), 0.04951, 0.04951, 0.04951, 0.04951, 0.04951, 0)
         self.assertEqual(result, self.ds.getData("btc")[0])
-
-    def test_get_data_in_range(self):
-        result = [(datetime.date(2010, 7, 17), 0.04951, 0.04951, 0.04951, 0.04951, 0.04951, 0), (datetime.date(2010, 7, 18), 0.04951, 0.08585, 0.05941, 0.08584, 0.08584, 5)]
-        self.assertEqual(result, self.ds.getDataInRange(self.datasetRange, 20100717, 20100718))
-
-    def test_get_data_of_type(self):
-        result = [(datetime.date(2010, 7, 17), 0.04951), (datetime.date(2010, 7, 18), 0.04951)]
-        self.assertEqual(result, self.ds.getDataOfType(self.datasetType, "openprice"))
 
     def test_perform_data_query(self):
         result = [[(datetime.date(2010, 7, 17), 0.04951), (datetime.date(2010, 7, 18), 0.04951)], [(datetime.date(2010, 7, 17), 0.04951), (datetime.date(2010, 7, 18), 0.04951)]]

f5f0424ea32cbe84f90413f9b1ce43be371ac387
yelouis <chengluye@gmail.com>
2019-11-06 12:48:49 -0600

    Put numpy back in and added revision file

--- backend/datasource.py
+++ backend/datasource.py
@@ -1,7 +1,7 @@
 import psycopg2
 import getpass
 import datetime
-# import numpy as np
+import numpy as np
 # from sklearn.linear_model import LinearRegression
 # from sklearn.model_selection import train_test_split
 

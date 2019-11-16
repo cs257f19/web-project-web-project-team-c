01f016503ac2934cadf35ecb2ed6ef88ed0a7e47
Louis Ye <44011239+yelouis@users.noreply.github.com>
2019-11-16 15:22:25 -0600

    trying to format?

--- webapp.py
+++ webapp.py
@@ -59,8 +59,8 @@ def regression(dataset1, dataset2, datatype1, datatype2, ds):
     firstDate = 19600104
     firstDate = 20191007
     today = 20191008
-    returndata1 = ds.performDataQuery([dataset1], datatype1, firstDate, today)
-    returndata2 = ds.performDataQuery([dataset2], datatype2, firstDate, today)
+    returndata1 = [ds.performDataQuery([dataset1], datatype1, firstDate, today)]
+    returndata2 = [ds.performDataQuery([dataset2], datatype2, firstDate, today)]
     print("returndata", [dataset1, dataset2])
     returndata = ds.formatData([dataset1, dataset2])
     print("returndata", returndata)

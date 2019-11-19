26ee7353a7aaba57f5a9f502c677b6c502aad289
nhayes-rich <nhayesrich@gmail.com>
2019-11-18 22:07:28 -0600

    moved old files, added analysis html page, added flask for analysis results

--- webapp.py
+++ webapp.py
@@ -8,8 +8,9 @@ from datasource import DataSource
 import datetime
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from io import StringIO
 from matplotlib import pyplot as plt
-
+import base64
 
 app = flask.Flask(__name__)
 
@@ -19,7 +20,7 @@ def index():
 
     today = ds.dateTimeToStr(datetime.datetime.today().date())
     today = ds.strToInt(today)
-    # If the tables where constantly updated from yahooFiance. Because our tables are
+    # If the tables were constantly updated from yahooFiance. Because our tables are
     # not constantly updated. We will have place holder to have the most recent price
 
     today = 20191008
@@ -27,21 +28,14 @@ def index():
     returndata = ds.performDataQuery(['spy', 'btc', 'gld', 'irx'], 'adjcloseprice', today-1, today)
     returndata = ds.formatData(returndata)
     listOfreturnHTML = makePriceChangeBetweenTwoDaysHTML(returndata)
-    if request.method == 'GET':
-        return render_template('index.html', listOfreturnHTML=listOfreturnHTML)
-    if request.method == 'POST':
-        dataset1 = request.form.get('dataset1')
-        dataset2 = request.form.get('dataset2')
-        datatype1 = request.form.get('datatype1')
-        datatype2 = request.form.get('datatype2')
-        regression(dataset1, dataset2, datatype1, datatype2, ds)
 
+    return render_template('index.html', listOfreturnHTML=listOfreturnHTML)
 
-'''
-Writes the html code to update the current prices and the color of the current price to
-indicate positive or negative change in price
-'''
 def makePriceChangeBetweenTwoDaysHTML(returndata):
+    '''
+    Writes the html code to update the current prices and the color of the current 
+    price to indicate positive or negative change in price
+    '''
     listOfreturnHTML = []
     for datasetIndex in range(1,len(returndata[0])):
         if returndata[1][datasetIndex] - returndata[0][datasetIndex] < 0:
@@ -61,6 +55,9 @@ def regression(dataset1, dataset2, datatype1, datatype2, ds):
     returndata2 = ds.performDataQuery([dataset2], datatype2, firstDate, today)
     returndata = ds.formatData([returndata1[0], returndata2[0]])
 
+    if returndata == [[], []]:
+        return (0, 0, [[], []])
+
     xValueList = []
     yValueList = []
     for tupleIndex in range(len(returndata)):
@@ -71,8 +68,9 @@ def regression(dataset1, dataset2, datatype1, datatype2, ds):
     reg = LinearRegression().fit(xValueList, yValueList)
     plt.scatter(xValueList, yValueList,color='g')
     plt.plot(xValueList, reg.predict(xValueList),color='k')
-    print("Got here")
-    plt.show()
+    image = StringIO()
+    plt.savefig(image, format='png')
+    return (base64.encodestring(image.getValue()), reg.predict(xValueList), returndata)
 
 @app.route("/results.html", methods=['GET','POST'])
 def result():
@@ -93,6 +91,23 @@ def result():
         returnhtml = flask.Markup(returnhtml)
 
         return render_template('results.html', returnhtml=returnhtml, returndata=returndata, dataset1=dataset1, dataset2=dataset2, datatype=datatype)
+    else:
+        returnhtml = "<h2>Query Failed</h2>"
+        returnhtml = flask.Markup(returnhtml)
+        return render_template('results.html', returnhtml=returnhtml)
+
+@app.route("/analysis.html", methods=['GET','POST'])
+def analysisresults():
+    if request.method == 'POST':
+        ds = DataSource('hayesrichn', 'orange227blue')
+        dataset1 = request.form.get('dataset1')
+        datatype1 = request.form.get('datatype1')
+        dataset2 = request.form.get('dataset2')
+        datatype2 = request.form.get('datatype2')
+
+        returndata = regression(dataset1, dataset2, datatype1, datatype2, ds)
+        formatted_dataset = ds.formatData(returndata[2])
+        return render_template('analysis.html', result=returndata[0], predicted_value=returndata[1], datatype1=datatype1, dataset1=dataset1, datatype2=datatype2, dataset2=dataset2, data=formatted_dataset)
     else:
         returnhtml = "<h2>Query Failed</h2>"
         returnhtml = flask.Markup(returnhtml)

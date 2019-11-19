72905fd92aa8b64147f3fb2d03cc4425b65586d1
nhayes-rich <nhayesrich@gmail.com>
2019-11-19 00:03:21 -0600

    Update datasource.py

--- datasource.py
+++ datasource.py
@@ -158,9 +158,9 @@ class DataSource:
     	'''
         try:
             cursor = self.connection.cursor()
-            query = "SELECT pricedate, %s FROM {0} WHERE pricedate BETWEEN to_date(%s::text, 'YYYYMMDD') AND to_date(%s::text, 'YYYYMMDD')".format(AsIs(quote_ident(setname, cursor)))
-            print(cursor.mogrify(query, (dataType, fromDate, toDate, )))
-            cursor.execute(query, (dataType, fromDate, toDate, ))
+            query = "SELECT pricedate, %s FROM %s WHERE pricedate BETWEEN to_date(%s::text, 'YYYYMMDD') AND to_date(%s::text, 'YYYYMMDD')"
+            print(cursor.mogrify(query, (dataType, setname, fromDate, toDate, )))
+            cursor.execute(query, (dataType, setname, fromDate, toDate, ))
             return cursor.fetchall()
         except Exception as e:
             print("Error while executing query: ", e)
